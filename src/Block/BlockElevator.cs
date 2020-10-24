9f51a14bd749153619eac80afbeb93ca96550d1c
DArkHekRoMaNT <darkhekromant@yandex.ru>
2020-10-24 22:00:54 +0300

    Initial commit

---
+++ src/Block/BlockElevator.cs
@@ -0,0 +1,41 @@
+using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+
+namespace SimpleElevator
+{
+    public class BlockElevator : Block
+    {
+        public int GetRange()
+        {
+            return Attributes["range"][LastCodePart(1)].AsInt() * (LastCodePart(0) == "temporal" ? 2 : 1);
+        }
+        public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact)
+        {
+            base.OnEntityCollide(world, entity, pos, facing, collideSpeed, isImpact);
+
+            if (facing == BlockFacing.UP)
+            {
+                BlockEntityElevator be = world.BlockAccessor.GetBlockEntity(pos) as BlockEntityElevator;
+                if (be == null) return;
+                be.OnEntityCollide(entity);
+            }
+        }
+
+        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+
+            dsc.AppendLine(Lang.Get(
+                SimpleElevator.MOD_ID + ":heldhelp-material",
+                Lang.Get("material-" + LastCodePart(1))
+            ));
+            dsc.AppendLine(Lang.Get(
+                SimpleElevator.MOD_ID + ":heldhelp-range",
+                Attributes["range"][LastCodePart(1)].AsInt() * (LastCodePart(0) == "temporal" ? 2 : 1)
+            ));
+        }
+    }
+}
---
+++ src/BlockEntity/BEElevator.cs
@@ -0,0 +1,74 @@
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace SimpleElevator
+{
+    class BlockEntityElevator : BlockEntity
+    {
+
+        long lastCollideMs;
+        Entity lastEntity;
+        int range;
+        public override void Initialize(ICoreAPI api)
+        {
+            base.Initialize(api);
+
+            if (api.Side == EnumAppSide.Server)
+            {
+                if (Block?.Attributes != null)
+                {
+                    range = (Block as BlockElevator).GetRange();
+                    RegisterGameTickListener(OnServerGameTick, 50);
+                }
+            }
+        }
+
+        internal void OnEntityCollide(Entity entity)
+        {
+            if (lastEntity != entity) lastEntity = entity;
+            lastCollideMs = Api.World.ElapsedMilliseconds;
+        }
+        public void OnServerGameTick(float dt)
+        {
+            if (lastEntity != null)
+            {
+                if (Api.World.ElapsedMilliseconds - lastCollideMs > 300)
+                {
+                    lastEntity = null;
+                    return;
+                }
+                if (Api.Side == EnumAppSide.Server)
+                {
+                    if ((lastEntity as EntityAgent).Controls.Sneak)
+                    {
+                        for (int i = 1; i <= range; i++)
+                        {
+                            if (TryTP(-i)) break;
+                        }
+                    }
+                    else if ((lastEntity as EntityAgent).Controls.Jump)
+                    {
+                        for (int i = 1; i <= range; i++)
+                        {
+                            if (TryTP(i)) break;
+                        }
+                    }
+                }
+            }
+        }
+
+        internal bool TryTP(int shiftY)
+        {
+            BlockPos newPos = new BlockPos(Pos.X, Pos.Y + shiftY, Pos.Z);
+            Block sec = Api.World.BlockAccessor.GetBlock(newPos);
+            if (sec.GetType() == Type.GetType(SimpleElevator.MOD_SPACE + ".BlockElevator"))
+            {
+                lastEntity.TeleportTo(new Vec3d(newPos.X, newPos.Y, newPos.Z).Add(.5, 1, .5));
+                return true;
+            }
+            return false;
+        }
+    }
+}
